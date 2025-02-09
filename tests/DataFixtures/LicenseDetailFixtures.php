42c4829b4217daed8b8febed53bcf7dfe7ace106
Dwalyn <jerome.blisac@gmail.com>
2025-02-09 21:52:57 +0100

    correction 5

---
+++ tests/DataFixtures/LicenseDetailFixtures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\DataFixtures;
+
+use App\DataFixtures\LicenseDetailFixtures as Fixtures;
+use App\Tests\Traits\FixtureDirectoryTrait;
+
+class LicenseDetailFixtures extends Fixtures
+{
+    use FixtureDirectoryTrait;
+}
---
+++ tests/DataFixtures/LicenseFixtures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\DataFixtures;
+
+use App\DataFixtures\LicenceFixtures as Fixtures;
+use App\Tests\Traits\FixtureDirectoryTrait;
+
+class LicenseFixtures extends Fixtures
+{
+    use FixtureDirectoryTrait;
+}
