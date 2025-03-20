1d0b3a466b68c13c42bf05b8396eb345da31a600
Dwalyn <jerome.blisac@gmail.com>
2025-03-20 21:21:26 +0100

    correction UserStatusTest

--- tests/Twig/Components/User/UserStatusTest.php
+++ tests/Twig/Components/User/UserStatusTest.php
@@ -17,33 +17,46 @@ class UserStatusTest extends WebTestCase
     }
 
     #[DataProvider('userStatusProvider')]
-    public function testUserStatus(string $email, bool $userStatus, bool $userStatusAfter): void
+    public function testUserStatus(string $email, bool $userStatus, bool $resultStatus): void
     {
-        $this->login('admin@google.com');
+        $userLogin = $this->getRepository(User::class)->findOneBy(['email' => 'admin@google.com']);
         $user = $this->getRepository(User::class)->findOneBy(['email' => $email]);
         $this->assertEquals($user->isEnable(), $userStatus);
 
         $testComponent = $this->createLiveComponent(
             name: 'Administration:User:UserStatus',
-            data: ['user' => $user, 'enable' => $userStatusAfter],
+            data: [
+                'user' => $user,
+                'enable' => $resultStatus,
+            ],
         );
-
+        $testComponent->actingAs($userLogin);
         $testComponent->call('changeEnable');
+
         $user = $this->getRepository(User::class)->findOneBy(['email' => $email]);
-        $this->assertEquals($user->isEnable(), $userStatusAfter);
+        if ($user->getUserIdentifier() === $userLogin->getUserIdentifier()) {
+            $this->assertEquals($user->isEnable(), !$resultStatus);
+        } else {
+            $this->assertEquals($user->isEnable(), $resultStatus);
+        }
     }
 
     public static function userStatusProvider(): \Generator
     {
-        yield 'enable_user' => [
+        yield 'disable_user' => [
             'email' => 'test2@google.com',
             'userStatus' => false,
-            'userStatusAfter' => true,
+            'resultStatus' => true,
         ];
         yield 'disable_user' => [
             'email' => 'test@google.com',
             'userStatus' => true,
-            'userStatusAfter' => false,
+            'resultStatus' => false,
+        ];
+        yield 'impossible_change_status' => [
+            'email' => 'admin@google.com',
+            'userStatus' => true,
+            'resultStatus' => false,
         ];
     }
 }
