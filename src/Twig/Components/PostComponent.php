4073a684a0bcbf0c5cd9a3604a41633678404143
Dwalyn <jerome.blisac@gmail.com>
2025-06-21 21:13:30 +0200

    truncate description

--- src/Twig/Components/PostComponent.php
+++ src/Twig/Components/PostComponent.php
@@ -9,14 +9,23 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 class PostComponent
 {
     protected Post $post;
+    public bool $hasTruncateDescription;
 
-    public function mount(Post $post): void
-    {
+    public function mount(
+        Post $post,
+        bool $hasTruncateDescription = false,
+    ): void {
         $this->post = $post;
+        $this->hasTruncateDescription = $hasTruncateDescription;
     }
 
     public function getPost(): Post
     {
         return $this->post;
+    }
+
+    public function getTruncateDescription(): string
+    {
+        return sprintf('%s ...', substr($this->post->getDescription(), 0, 300));
     }
 }
