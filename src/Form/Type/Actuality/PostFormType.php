fa95c7977272851bf6c96a2d10ada95b96a42734
Dwalyn <jerome.blisac@gmail.com>
2025-06-29 21:50:46 +0200

    installation quill wiziwig

--- src/Form/Type/Actuality/PostFormType.php
+++ src/Form/Type/Actuality/PostFormType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type\Actuality;
 
 use App\Form\Datas\Actuality\PostFormData;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +13,13 @@ class PostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('content', TextareaType::class);
+        $builder->add('content', HiddenType::class, [ // Ou TextareaType si vous voulez un fallback sans JS
+            'attr' => [
+                'id' => 'your_entity_content_quill', // ID unique pour ce champ
+            ],
+            'label' => false, // Cache le label par défaut si vous le gérez manuellement dans Twig
+            'required' => false, // Adaptez selon vos besoins
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
